f619e69794682bc44bc91efc3fae0d4b35085066
Emil Bylykbashi <emilbylykbashi@gmail.com>
2020-03-11 13:21:22 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+//import LoggedOut from "./src/screens/LoggedOut";
+
+
+
+export default class App extends Component {
+  render() {
+    return <TextView />;
+  }
+}
